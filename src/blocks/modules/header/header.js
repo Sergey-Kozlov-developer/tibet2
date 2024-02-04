d17b1dc3161a4d1c79c5c48b5bae510c8ce25cc5
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2024-02-04 17:01:23 +0300

    tbet2

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,8 @@
+// const btn = document.querySelector(".btn");
+// console.log(btn);
+
+// btn.addEventListener("click", changedColor);
+
+// function changedColor(event) {
+// 	event.target.style.color = "red";
+// }
